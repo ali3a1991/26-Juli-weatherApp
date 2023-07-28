5465872481fa4d9aedf31a98c6f7d7a940252f05
ali3a1991 <ali3job@gmail.com>
2023-07-28 10:06:04 +0200

    update 01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,31 @@
 "use strict"
 
+// # =============================================== IMPORT
 import { apiKey } from "./api.js";
 
+// # ========================================== DECLARATION
 const button = document.getElementById('button');
 const cityNameInput = document.getElementById('cityNameInput');
 const cityInfoOutput = document.body.querySelector('.cityInfoOutput');
 const weatherInfo = document.body.querySelector('.weatherInfo');
 
-const hour = document.querySelector('.hour');
-const min = document.querySelector('.min');
-const sec = document.querySelector('.sec');
+const hourHand = document.querySelector('.hour-hand');
+const minHand = document.querySelector('.min-hand');
+const secHand = document.querySelector('.sec-hand');
 const date = document.querySelector('.date');
 
 const dayList = document.querySelectorAll('.dayList');
 
-
-// # ================ General
-dayList[0].classList = 'listSelected'
-let cityName = 'Berlin';
-cityLoction(cityName)
-
-
-
-// # ================ ANALOG CLOCK
+// # ================================== ANALOG CLOCK & DATE
 setInterval(() => {
     const myDate = new Date()
     const hourDeg = myDate.getHours()
     const minDeg = myDate.getMinutes()
     const secDeg = myDate.getSeconds()
 
-    sec.style.transform = `rotateZ(${secDeg * 6}deg)`
-    min.style.transform  = `rotateZ(${minDeg * 6}deg)`
-    hour.style.transform  = `rotateZ(${hourDeg * 30 + minDeg * 6 / 12}deg)`
+    secHand.style.transform = `rotateZ(${secDeg * 6}deg)`
+    minHand.style.transform  = `rotateZ(${minDeg * 6}deg)`
+    hourHand.style.transform  = `rotateZ(${hourDeg * 30 + minDeg * 6 / 12}deg)`
 
     let minutes;
     let seconds;
@@ -43,14 +37,12 @@ setInterval(() => {
         seconds = `${myDate.getSeconds()}`
     }
 
-    
     if (myDate.getMinutes() < 10) {
         minutes = `0${myDate.getMinutes()}`
     }else{
         minutes = `${myDate.getMinutes()}`
     }
 
-    
     if (myDate.getHours() < 10) {
         hours = `0${myDate.getHours()}`
     }else{
@@ -60,19 +52,52 @@ setInterval(() => {
     <h1>${myDate.toDateString()}</h1>
     <h2>${hours} : ${minutes} : ${seconds}</h2>
     `
-
-    
 }, 1000)
 
+// # ========================================== WEATHER APP
 
+// =========================================== Default Data
+dayList[0].classList = 'listSelected'
+let cityName = 'Berlin';
+let geoApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},de&appid=${apiKey}`
+cityLoction(cityName)
+
+// =========================================== Coming Days
 for (let i = 1; i < 6; i++) {
-    const d = new Date()
-    d.setDate(d.getDate() + i);
-    dayList[i].textContent = `${d.toDateString()}`
+    const nowDate = new Date()
+    nowDate.setDate(nowDate.getDate() + i);
+    dayList[i].textContent = `${nowDate.toDateString()}`
 }
 
+dayList.forEach(list => {
+    list.addEventListener('click', (event) => {
+        dayList.forEach(li => li.classList= 'dayList')
+        event.target.classList = 'listSelected'
+        let listValue = list.value
+        if (listValue == -1) {
+            cityLoction(cityName)
+        }else{
+            cityLoction2(cityName, listValue)
+        }
+    })
+})
 
-// # ================ WEATHER APP
+function cityLoction2(cityNamePara, listValue) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityNamePara},de&appid=${apiKey}`)
+        .then(response => {
+            return response.json()
+        })
+        .then(location => {
+            cityInfoOutput.innerHTML =`
+            <h1>Weather in ${location[0].name}</h1>
+            <h3>${location[0].state} / ${location[0].country}</h3>
+            `;
+            const cityInfoPara = `http://api.openweathermap.org/data/2.5/forecast?lat=${location[0].lat}&lon=${location[0].lon}&cnt=5&appid=${apiKey}`
+            cityInfo2(cityInfoPara, listValue)
+        })
+        .catch(error => console.log(error));
+}
+
 button.addEventListener('click', () => {
     dayList.forEach(li => li.classList= 'dayList')
     dayList[0].classList = 'listSelected'
@@ -96,37 +121,6 @@ document.body.addEventListener('keypress', (event) => {
         cityLoction(cityName)
     }
 })
-
-dayList.forEach(list => {
-    list.addEventListener('click', (event) => {
-        dayList.forEach(li => li.classList= 'dayList')
-        event.target.classList = 'listSelected'
-        let listValue = list.value
-        if (listValue == -1) {
-            cityLoction(cityName)
-        }else{
-            cityLoction2(cityName, listValue)
-        }
-    })
-})
-
-
-function cityLoction2(cityNamePara, listValue) {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityNamePara},de&appid=${apiKey}`)
-        .then(response => {
-            return response.json()
-        })
-        .then(location => {
-            cityInfoOutput.innerHTML =`
-            <h1>Weather in ${location[0].name}</h1>
-            <h3>${location[0].state} / ${location[0].country}</h3>
-            `;
-            const cityInfoPara = `http://api.openweathermap.org/data/2.5/forecast?lat=${location[0].lat}&lon=${location[0].lon}&cnt=5&appid=${apiKey}`
-            cityInfo2(cityInfoPara, listValue)
-        })
-        .catch(error => console.log(error));
-}
-
 
 function cityLoction(cityNamePara) {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityNamePara},de&appid=${apiKey}`)
